32e62a99e8d16b9426c90a957c771970417ad457
herohead049 <herohead@gmail.com>
2015-07-01 15:30:32 -0400

    added getEmailFrom RMQ

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ var redis = require("redis");
 var webServer = new Hapi.Server({ connections: { routes: { cors: { origin: ['http://backupreport.eu.mt.mtnet'] } } } });
 webServer.connection({ port: 8000 });
 
+var processDetails = {
+    name: "",
+    description: ""
+};
+
+
 // chalk object
 
 var c = {
@@ -82,20 +88,13 @@ var getSaveGroup = function (rabbitMQ) {
 
 //*** setup getSaveGroup process
 
-var sgRabbitMQ = _.clone(rabbitMQ, true);
-sgRabbitMQ.queue = 'nw.savegroup';
-getSaveGroup(sgRabbitMQ);
 
 //---------------------------
 
 
 
-var webHits = function () {
-    var webHitsName = "webHits";
-
-    writeConsole(c.info, webHitsName, " process started");
-
-
+var webHits = function (processDetails) {
+    writeConsole(c.info, processDetails.name, " process started");
     webServer.route({
         method: 'GET',
         path: '/php/{data}',
@@ -105,23 +104,19 @@ var webHits = function () {
             fs.appendFile('newWebAccess.log', moment().format() + "," + site.webSite + "," + site.ip + "," + site.page + "," + site.duration + "," + site.link + "\n", function (err) {
                 if (err) { throw err; }
                 console.log(moment().format(), site.webSite, site.ip, site.page, site.duration, site.link);
+                writeConsole(c.info, processDetails.name, site.webSite + ":" + site.ip + ":" + site.page + ":" + site.duration + ":" + site.link);
                 //console.log('The "data to append" was appended to file!');
             });
             reply('Thanks for the information that you uploaded.');
         }
     });
     // Start the server
-
-
 };
-
-webHits();
 
 //*** emailLookup
 
-var emailLookup = function (redisConf) {
-    var emailLookUpName = "emailLookup",
-        emailLookupRedisClient = redis.createClient(redisConf.port, redisConf.server),
+var emailLookup = function (redisConf, processDetails) {
+    var emailLookupRedisClient = redis.createClient(redisConf.port, redisConf.server),
         addEmail = function (redisKey, key, val) {
             console.log('Added', val);
             emailLookupRedisClient.hset(redisKey, key, val);
@@ -135,11 +130,11 @@ var emailLookup = function (redisConf) {
                 callback(JSON.stringify(reply));
             });
         };
-    writeConsole(c.info, emailLookUpName, " process started");
+    writeConsole(c.info, processDetails.name, " process started");
     emailLookupRedisClient.on("error", function (err) {
         console.log("Error " + err);
     });
-    writeConsole(c.info, emailLookUpName, 'Connected to :' + redisConf.server);
+    writeConsole(c.info, processDetails.name, 'Connected to :' + redisConf.server);
 
     webServer.route({
         method: 'GET',
@@ -154,7 +149,7 @@ var emailLookup = function (redisConf) {
         path: '/get/emails',
         handler: function (request, reply) {
             getEmail("emailKey", function (em) {
-                console.log("sending emails", em);
+                writeConsole(c.standard, processDetails.name, "sending emails  " +  em);
                 reply(em);
             });
         }
@@ -183,10 +178,70 @@ var emailLookup = function (redisConf) {
     });
 };
 
+var getEmailFromRabbit = function (rabbitMQ, processDetails) {
+    writeConsole(c.info, processDetails.name, " process started");
+    var rabbitMQAuthString = 'amqp://' + rabbitMQ.username + ':' + rabbitMQ.password + '@' + rabbitMQ.server + rabbitMQ.virtualHost;
+    amqp.connect(rabbitMQAuthString).then(function (conn) {
+        process.once('SIGINT', function () { conn.close(); });
+        return conn.createChannel().then(function (ch) {
+            var ok = ch.assertQueue(rabbitMQ.queue, {durable: true});
+            ok = ok.then(function (_qok) {
+                return ch.consume(rabbitMQ.queue, function (msg) {
+                    var emailServer = JSON.parse(msg.content);
+                    //console.log(moment().format(), emailServer.to, emailServer.from, emailServer.subject);
+                    writeConsole(c.info, processDetails.name, emailServer.to + ":" + emailServer.from + ":" + emailServer.subject);
+
+                    if (emailServer.type === 'html') {
+                        cdlibjs.sendEmailHtml(emailServer);
+                        ch.ack(msg);
+                    }
+                }, {noAck: false});
+            });
+            return ok.then(function (_consumeOk) {
+                writeConsole(c.success, processDetails.name, ' [*] Waiting for messages from quename "' + rabbitMQ.queue + '" To exit press CTRL+C');
+                //console.log(' [*] Waiting for messages. To exit press CTRL+C');
+            });
+        });
+    }).then(null, console.warn);
+
+};
+
+
+//** setup process
+
+var emailRabbitProcess = _.clone(processDetails, true);
+emailRabbitProcess.name = "Email Rabbit collection";
+emailRabbitProcess.description = "This will get the emails from rabbit to sent";
+//----------------------------
+var webHitsProcess = _.clone(processDetails, true);
+webHitsProcess.name = "webHits";
+webHitsProcess.description = "Web tracking process";
+var emailRabbit = _.clone(rabbitMQ, true);
+emailRabbit.queue = 'notifications.email';
+
+//---------------------------
+
+var sgRabbitMQ = _.clone(rabbitMQ, true);
+sgRabbitMQ.queue = 'nw.savegroup';
+
+//------------------
+var emaillookupProcess = _.clone(processDetails, true);
+emaillookupProcess.name = "Email lookup";
+emaillookupProcess.description = "retrieve emails from Redis";
 var emailLookupRedisConf = _.clone(redisConf);
-emailLookup(emailLookupRedisConf);
+
+
+
+// start webserver
 
 webServer.start(function () {
-    console.log('info', "Server running at", webServer.info.uri);
+    //console.log('info', "Server running at", webServer.info.uri);
+    writeConsole(c.info, "General webserver", "Started on " + webServer.info.uri);
 });
 
+//---- start processes
+
+webHits(webHitsProcess);
+getEmailFromRabbit(emailRabbit, emailRabbitProcess);
+getSaveGroup(sgRabbitMQ);
+emailLookup(emailLookupRedisConf, emaillookupProcess);
